7c269055ac4a2b807f04d1398af98acb12d8ac0b
Lloyd Banner <lmb565@york.ac.uk>
2018-11-14 15:59:16 +0000

    eclipse project

---
+++ SEPR/core/src/com/mygdx/game/MyGdxGame.java
@@ -0,0 +1,42 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mygdx.game.screens.Play;
+
+public class MyGdxGame extends Game {
+	
+	@Override
+	public void create () {
+		setScreen(new Play());
+	}
+
+	@Override
+	public void render () {
+		super.render();
+	}
+	
+	@Override
+	public void dispose () {
+		super.dispose();
+	}
+	
+	@Override
+	public void resize (int width, int height) {
+		super.resize(width, height);
+	}
+	
+	@Override
+	public void pause() {
+		super.pause();
+	}
+	
+	@Override
+	public void resume() {
+		super.resume();
+	}
+}
